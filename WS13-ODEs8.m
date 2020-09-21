3e4709ff144c7e4d2842406251ad1fe1e54e49a7
JP <jhphillips1029@gmail.com>
2020-09-21 11:57:28 -0600

    D16

---
+++ WS13-ODEs8.m
@@ -0,0 +1,50 @@
+% ODE Worksheet 8 - Matrix approach
+% clear; clc
+
+% Given
+T1 = 40;
+T2 = 200;
+Ta = 20;
+k = 0.01;
+L = 10;
+
+N = 4;
+dx = L/N;
+
+Y1 = T1;
+YN = T2;
+C_curr = -k;
+C_prev = 0;
+C_next = 0;
+C_B = -k*Ta;
+
+A = zeros(N,N);
+B = zeros(N,1);
+
+A(1,1) = 1;
+for j=2:N
+	A(1,j) = 0;
+end
+A(N,N) = 1;
+for j=1:N-1
+	A(N,j) = 0;
+end
+for i=2:N-1
+	A(i,i-1) = 1/dx^2 + C_prev;
+	A(i,i) = -2/dx^2 + C_curr;
+	A(i,i+1) = 1/dx^2 + C_next;
+end
+
+B(1) = Y1;
+B(N) = YN;
+for i=2:N-1
+	B(i) = C_B;
+end
+
+u = inv(A)*B;
+
+x = linspace(0,L,N);
+plot(x,u,'DisplayName','Numerical');
+drawnow;
+
+wait = input("Press Enter to Exit.");
